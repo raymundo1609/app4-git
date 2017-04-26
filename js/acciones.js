6d666f3649f9b139974c838666997c38b3306c2e
raymundo1609 <raymundo.vazquez.rojas.cbtis16@gmail.com>
2017-04-26 14:23:54 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -2,11 +2,11 @@
 
  $(document).ready(function (e){
 	 document .addEventListener ("deviceready",function(){
-		 $('#disp table td').eq(2).text(device.model);
+		 $('#disp table td').eq(3).text(device.model);
 		 $('#disp table td').eq(5).text(device.cordova);
-		 $('#disp table td').eq(7).text(device.plataform);
+		 $('#disp table td').eq(7).text(device.platform);
 		 $('#disp table td').eq(9).text(device.version);
-		 $('#disp table td').eq(11).text(device.uui);
+		 $('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause",function(){
 		escribehistoria ("la app se pauso");
 	},false);//pause
