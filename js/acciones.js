7b66f2f1a360f8654b5ffa031914a313d9122ea8
raymundo1609 <raymundo.vazquez.rojas.cbtis16@gmail.com>
2017-04-27 22:03:27 -0500

    10subida

--- js/acciones.js
+++ js/acciones.js
@@ -8,7 +8,7 @@ $(document).ready(function (e){
 	  $('#disp table td').eq(9).text(device.version);
 	  $('#disp table td').eq(11).text(device.uuid);
 document.addEventListener("pause",function(){
-	escribehistoria8('la app se pauso');
+	escribehistoria('la app se pauso');
 },false);//pause
 document.addEventListener("resume",function(){
 	escribehistoria('la app se reinicio');
